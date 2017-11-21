02eeba940128bf5a4c414e46c5388e7009f4976a
Masaya Matsukawa <e175708@ie.u-ryukyu.ac.jp>
2017-11-21 11:06:45 +0900

    Step4終了

---
+++ src/jp/ac/uryukyu/ie/ex3/pair17/Dice.java
@@ -0,0 +1,21 @@
+package jp.ac.uryukyu.ie.ex3.pair17;
+
+public class Dice{
+    private int value;
+
+    public void Dice(){
+	play();
+	    }
+
+    public int getValue(){
+	return value;
+    }
+
+    public void setValue(int value){
+	this.value = value;
+    }
+
+    public void play(){
+	this.value = (int)(Math.random()*6)+1;
+    }
+}
